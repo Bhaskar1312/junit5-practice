7eb0dae7b1b50f73c7747b806390f3e7cba6becb
Bhaskar Bantupalli <bhaskarbnms0@gmail.com>
2024-10-05 18:47:37 +0530

    Test passes

--- src/test/java/com/example/junit5practice/ParametrizedTests.java
+++ src/test/java/com/example/junit5practice/ParametrizedTests.java
@@ -3,10 +3,13 @@ package com.example.junit5practice;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 // https://nipafx.dev/junit-5-parameterized-tests/
 public class ParametrizedTests {
+
     @ParameterizedTest
+    @ValueSource(strings={"hello", "world"})
     void parameterizedTest(String word) {
         assertNotNull(word);
     }
