fe40c7a7cd32879ae91e2f4fc092fcee406fdf24
Bhaskar Bantupalli <bhaskarbnms0@gmail.com>
2024-10-05 19:00:23 +0530

    meta annotations

--- src/test/java/com/example/junit5practice/ParametrizedTests.java
+++ src/test/java/com/example/junit5practice/ParametrizedTests.java
@@ -2,7 +2,12 @@ package com.example.junit5practice;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.TestReporter;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -23,5 +28,22 @@ class ParametrizedTests {
 
     }
 
+    @ParameterizedTest
+    @ValueSource(strings={"hello", "world"})
+    void withOtherParams(String word, TestInfo info, TestReporter reporter) {
+        reporter.publishEntry(info.getDisplayName(), "Word: " + word);
+    }
+
+
+    // meta annotations
+    @Retention(RetentionPolicy.RUNTIME)
+    @ParameterizedTest(name = "Elaborate name listing all {arguments}")
+    @ValueSource(strings={"hello", "world"})
+    public @interface Params {}
+
+    @Params
+    void testMetaAnnotation(String word) {
+        assertNotNull(word);
+    }
 
 }
