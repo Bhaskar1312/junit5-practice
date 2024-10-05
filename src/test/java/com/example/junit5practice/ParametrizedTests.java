4b3d65dd4d8946980cdcf50ac1476645e0a707d1
Bhaskar Bantupalli <bhaskarbnms0@gmail.com>
2024-10-05 18:54:09 +0530

    csv source

--- src/test/java/com/example/junit5practice/ParametrizedTests.java
+++ src/test/java/com/example/junit5practice/ParametrizedTests.java
@@ -2,16 +2,25 @@ package com.example.junit5practice;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 // https://nipafx.dev/junit-5-parameterized-tests/
-public class ParametrizedTests {
+class ParametrizedTests {
 
-    @ParameterizedTest
+    @ParameterizedTest(name="run #{index} with [{arguments}]") // index starts at 1
     @ValueSource(strings={"hello", "world"})
     void parameterizedTest(String word) {
         assertNotNull(word);
+    }
+
+    @DisplayName("Roman numeral")
+    @ParameterizedTest(name ="\"{0}\" should be {1}")
+    @CsvSource({"I, 1", "II, 2", "III, 3"})
+    void withNiceName(String word, int number) {
+
     }
 
 
